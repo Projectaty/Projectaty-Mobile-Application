3458c6cb6a6176962ff6f0877db09e58182308a0
Sondos <sondosahmedabed@gmail.com>
2024-01-31 17:24:39 +0200

    Serach activity

---
+++ app/src/main/java/com/projectaty/activities/taskmanagement/SearchTask.java
@@ -0,0 +1,15 @@
+package com.projectaty.activities.taskmanagement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.projectaty.R;
+
+public class SearchTask extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.seatch_task);
+
+    }
+}

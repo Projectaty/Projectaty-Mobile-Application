9249288d415d851398dbc5427de4bb9030627b03
Sondos <sondosahmedabed@gmail.com>
2024-02-04 16:16:05 +0200

    remove unused imports

--- app/src/main/java/com/projectaty/activities/usermanagement/IntroductoryActivity.java
+++ app/src/main/java/com/projectaty/activities/usermanagement/IntroductoryActivity.java
@@ -3,7 +3,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;

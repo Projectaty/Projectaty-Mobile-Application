3a1454a85e2615f5e17ac8c9a8f9b8aec8637a6e
Sondos <sondosahmedabed@gmail.com>
2024-02-07 02:15:09 +0200

    Centralized Prefrence Class

--- app/src/main/java/com/projectaty/activities/usermanagement/IntroductoryActivity.java
+++ app/src/main/java/com/projectaty/activities/usermanagement/IntroductoryActivity.java
@@ -8,35 +8,27 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.projectaty.R;
+import com.projectaty.config.Prefrences;
 
 public class IntroductoryActivity extends AppCompatActivity {
-
+    /*
+        Custom Introductory Activity Based on user prefrences
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Check if the user has an account
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        boolean hasAccount = sharedPreferences.getBoolean("hasAccount", false);
-
-        // Check if the user chose to remember me
-        boolean rememberMe = sharedPreferences.getBoolean("rememberMe", false);
-
-        if (hasAccount) {
-            // User has an account, open Welcome Back activity
+        if (!Prefrences.isFirstTime(this)) {
             setContentView(R.layout.welcome_back);
             Button loginButton = findViewById(R.id.LoginButton);
             loginButton.setOnClickListener(e->{
                     startActivity(new Intent(IntroductoryActivity.this, LoginActivity.class));
             });
-
-            // Check if the user chose to remember me
-            if (rememberMe) {
+            if (Prefrences.isRememberMe(this)) {
                 startActivity(new Intent(IntroductoryActivity.this, StudentProfile.class));
             }
-
         } else {
-            // User doesn't have an account, open Get Started activity
+            Prefrences.setNotFirstTime(this);
             setContentView(R.layout.getting_started);
             Button createAccountButton = findViewById(R.id.createAccountButton);
             createAccountButton.setOnClickListener(e-> {

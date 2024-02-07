745fe815cc20ffe18cfcaa8b7539ef0e907365be
AFEFA KARAKRA <karakra.afifa123@gmail.com>
2024-02-07 15:39:41 +0200

    dashProject

---
+++ app/src/main/java/com/projectaty/activities/projectmanagment/ProjectDashboard.java
@@ -0,0 +1,16 @@
+package com.projectaty.activities.projectmanagment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.projectaty.R;
+
+public class ProjectDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

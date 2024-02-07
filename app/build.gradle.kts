b71a8fcd56ddd9de86e3829cb78fe7984bf675e9
AFEFA KARAKRA <karakra.afifa123@gmail.com>
2024-02-07 15:37:01 +0200

    gradleVersion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation ("androidx.recyclerview:recyclerview:1.2.0")
     implementation ("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")

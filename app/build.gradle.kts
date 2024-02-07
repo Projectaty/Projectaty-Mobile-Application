c22a889f9e67a6d05b5466280e9459cbd4ac06f5
Sondos <sondosahmedabed@gmail.com>
2024-02-07 02:07:09 +0200

    Add Volley Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.android.volley:volley:1.2.1")
     implementation ("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")

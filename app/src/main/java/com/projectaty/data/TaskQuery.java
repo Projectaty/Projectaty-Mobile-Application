4456bc300d8a1117363707ae8207e412da71c31a
Sondos <sondosahmedabed@gmail.com>
2024-02-07 02:37:10 +0200

    Create Queries Classes and URLs

---
+++ app/src/main/java/com/projectaty/data/TaskQuery.java
@@ -0,0 +1,5 @@
+package com.projectaty.data;
+
+public class TaskQuery {
+
+}
---
+++ app/src/main/java/com/projectaty/data/URLs.java
@@ -0,0 +1,26 @@
+package com.projectaty.data;
+
+public class URLs {
+    /*
+        Centralized URLs
+     */
+    private static final String GET_TASKS_URL = "";
+    private static final String ADD_TASK_URL = "";
+    private static final String UPDATE_TASK_URL = "";
+    private static final String DELETE_TASK_URL = "";
+
+    private static final String GET_USERS_URL = "";
+    private static final String ADD_USER_URL = "";
+    private static final String UPDATE_USER_URL = "";
+    private static final String DELETE_USER_URL = "";
+
+    private static final String GET_TEAMS_URL = "";
+    private static final String ADD_TEAM_URL = "";
+    private static final String UPDATE_TEAM_URL = "";
+    private static final String DELETE_TEAM_URL = "";
+
+    private static final String GET_PROJECTS_URL = "";
+    private static final String ADD_PROJECT_URL = "";
+    private static final String UPDATE_PROJECT_URL = "";
+    private static final String DELETE_PROJECT_URL = "";
+}

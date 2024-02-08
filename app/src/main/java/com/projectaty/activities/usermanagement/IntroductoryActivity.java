91008af3cf8ed850283677dae805beb40b2d27c8
hadeel120 <1202006@student.birzeit.edu>
2024-02-08 17:49:07 +0200

    update the code to check of the user hava accoiny or not

--- app/src/main/java/com/projectaty/activities/usermanagement/IntroductoryActivity.java
+++ app/src/main/java/com/projectaty/activities/usermanagement/IntroductoryActivity.java
@@ -1,4 +1,5 @@
 package com.projectaty.activities.usermanagement;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -16,15 +17,11 @@ public class IntroductoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Check if the user has an account
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         boolean hasAccount = sharedPreferences.getBoolean("hasAccount", false);
-
-        // Check if the user chose to remember me
         boolean rememberMe = sharedPreferences.getBoolean("rememberMe", false);
 
         if (hasAccount) {
-            // User has an account, open Welcome Back activity
             setContentView(R.layout.welcome_back);
             Button loginButton = findViewById(R.id.loginButton);
             loginButton.setOnClickListener(new View.OnClickListener() {
@@ -34,19 +31,19 @@ public class IntroductoryActivity extends AppCompatActivity {
                 }
             });
 
-            // Check if the user chose to remember me
             if (rememberMe) {
                 startActivity(new Intent(IntroductoryActivity.this, StudentProfile.class));
+                finish();
             }
 
         } else {
-            // User doesn't have an account, open Get Started activity
             setContentView(R.layout.getting_started);
             Button createAccountButton = findViewById(R.id.createAccountButton);
             createAccountButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     startActivity(new Intent(IntroductoryActivity.this, CreateAccount.class));
+                    finish();
                 }
             });
         }

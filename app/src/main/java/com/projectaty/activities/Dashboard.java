491f7426af4726b9828f359253b4aa92928d03c5
Sondos <sondosahmedabed@gmail.com>
2024-02-08 02:35:04 +0200

    Refactor: dashboead -> project_list.xml

--- app/src/main/java/com/projectaty/activities/Dashboard.java
+++ app/src/main/java/com/projectaty/activities/Dashboard.java
@@ -9,7 +9,7 @@ import com.projectaty.R;
 public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dashboard);
+        setContentView(R.layout.project_list);
 
     }
 

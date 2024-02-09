01f2091bad9e118859c5c45e24f52a59ee4e94bf
AFEFA KARAKRA <karakra.afifa123@gmail.com>
2024-02-09 18:58:53 +0200

    MenifistAndGradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,5 +46,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.0")
     implementation ("com.android.support.constraint:constraint-layout:1.1.3")
+    implementation("com.android.volley:volley:1.2.1")
 
 }

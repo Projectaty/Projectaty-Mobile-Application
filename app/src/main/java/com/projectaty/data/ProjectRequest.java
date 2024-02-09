c55df14fcbeaff688ca45b14f05f8e8defa3e4bc
AFEFA KARAKRA <karakra.afifa123@gmail.com>
2024-02-09 18:59:07 +0200

    PrjRequst

--- app/src/main/java/com/projectaty/data/ProjectRequest.java
+++ app/src/main/java/com/projectaty/data/ProjectRequest.java
@@ -1,4 +1,13 @@
 package com.projectaty.data;
 
+import android.content.ContentValues;
+
 public class ProjectRequest {
+
+
+    void addProjects (){
+
+        ContentValues cv = new ContentValues();
+
+    }
 }

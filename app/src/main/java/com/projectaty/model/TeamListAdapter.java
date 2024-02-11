3330dfc7e2ef8ca25cee7318fe2618ad383feadb
Rama <ramaabuadas@gmail.com>
2024-02-11 06:43:21 +0200

    volley updates commit

--- app/src/main/java/com/projectaty/model/TeamListAdapter.java
+++ app/src/main/java/com/projectaty/model/TeamListAdapter.java
@@ -1,13 +1,16 @@
 package com.projectaty.model;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.projectaty.R;
+import com.projectaty.activities.taskmanagement.UpdateDelTask;
 import com.projectaty.model.Team;
 
 import java.util.List;
@@ -25,6 +28,20 @@ public class TeamListAdapter extends ArrayAdapter<Team> {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.team_list_item, parent, false);
         }
+
+        TextView teamName = convertView.findViewById(R.id.teamItemName);
+
+        if (team != null) {
+            teamName.setText(team.getTeamName());
+        }
+
+        RelativeLayout teamLayout = convertView.findViewById(R.id.teamLayout);
+        teamLayout.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), UpdateDelTask.class);
+            intent.putExtra("TeamID", position);
+            intent.putExtra("TeamName",team.getTeamName() );
+            getContext().startActivity(intent);
+        });
 
         return convertView;
     }
